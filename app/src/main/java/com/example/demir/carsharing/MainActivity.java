40e3d0146cb98b9efaca99244add209296a81e00
demir005 <demirmah93@gmail.com>
2017-01-05 16:48:26 +0000

    Welcome Activity

--- app/src/main/java/com/example/demir/carsharing/MainActivity.java
+++ app/src/main/java/com/example/demir/carsharing/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.demir.carsharing;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,11 +11,13 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
     DatabaseHelper helper = new DatabaseHelper(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void onButtonClick(View v){
