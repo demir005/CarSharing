54b6df653650f022707e6fea1d2e62b8ee5b2e8f
demir005 <demirmah93@gmail.com>
2017-01-09 07:46:14 +0100

    Last commit
    Konacno aplikacija zavrsena. Pored problema sa SQLite-om odlucio sam da
    koristim lokalnu bazu i JSON

---
+++ app/src/main/java/com/Demir/CarSharing/Korisnik.java
@@ -0,0 +1,65 @@
+package com.Demir.CarSharing;
+
+import java.io.Serializable;
+
+/**
+ * Created by Azra on 26.08.2015..
+ */
+public class Korisnik implements Serializable{
+
+    private String id;
+    private String ime;
+    private String prezime;
+    private String email;
+    private String telefon;
+    private String fb;
+
+   Korisnik(String id, String ime, String prezime, String email, String telefon, String fb){
+       this.id = id;
+       this.ime = ime;
+       this.prezime = prezime;
+       this.email = email;
+       this.telefon = telefon;
+       this.fb = fb;
+   }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+
+    public String getFb() {
+        return fb;
+    }
+
+    public void setFb(String fb) {
+        this.fb = fb;
+    }
+}
